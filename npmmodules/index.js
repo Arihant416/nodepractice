498a0c660ed81fb316ed486f2351a7daf2e33d4f
Arihant416 <arihantjain416@gmail.com>
2021-03-29 14:09:17 +0530

    Command Line Basics

--- npmmodules/index.js
+++ npmmodules/index.js
@@ -1,2 +1,10 @@
+const getNotes = require('./notes')
 const chalk = require('chalk');
-console.log(chalk.black.bgRed('Error'));
+
+const command = process.argv[2];
+
+if(command === 'add'){
+  console.log('Adding A note');
+}else if(command ==="remove"){
+  console.log('Removing note')
+}
